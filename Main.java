44c08761ecb0b94a7879aea3fbe4ed578a66d23a
rababraza10 <ummerabab10@gmail.com>
2025-09-17 10:16:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-public class Main{
-  public static void main (String[] args){
-    System.out.println("hello, world!");
-  }
+// SmartBulb.java
+public interface SmartBulb {
+    void setPowerUsage(int value);  // Factory Method
+    int getPowerUsage();
+}
+
+
+
+// SmartLock.java
+public interface SmartLock {
+    void setBatteryConsumption(int value);  // Factory Method
+    int getBatteryConsumption();
 }
