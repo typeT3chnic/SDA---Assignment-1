bb260c3f63c9a3a73ec9e3e37a42cc03129fa87e
typeT3chnic <jaydentm1711@gmail.com>
2025-09-17 10:24:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public interface SmartBulb {
     int getPowerUsage();
 }
 
+//BrandABulb
+class BrandABulb implements SmartBulb{
+    private int pwrUsage;
+    public BrandABulb() {
+    }
+    
+    public void setPowerUsage(int p){
+        this.pwrUsage = p;
+    }
+    
+    public int getPowerUsage(){
+        return this.pwrUsage;
+    }
+    
+}//end brand A smart bulb
 
 
 // SmartLock.java
@@ -11,3 +26,20 @@ public interface SmartLock {
     void setBatteryConsumption(int value);  // Factory Method
     int getBatteryConsumption();
 }
+
+
+// BrandALock 
+class BrandALock implements SmartLock{
+    private int batt;
+    public BrandALock(){
+
+    }
+
+    public void setBatteryConsumption(int b){
+        this.batt = b;
+    }
+
+    public int getBatteryConsumption(){
+        return this.batt;
+    }
+}//end Brand A Lock
