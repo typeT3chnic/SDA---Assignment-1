4ad7186a0d2c5bb02388d4f7b6296d43746ab879
typeT3chnic <jaydentm1711@gmail.com>
2025-09-17 10:26:10 -0400

    Update BrandABulb.java

--- BrandABulb.java
+++ BrandABulb.java
@@ -1,1 +1,15 @@
-
+//BrandABulb
+class BrandABulb implements SmartBulb{
+    private int pwrUsage;
+    public BrandABulb() {
+    }
+    
+    public void setPowerUsage(int p){
+        this.pwrUsage = p;
+    }
+    
+    public int getPowerUsage(){
+        return this.pwrUsage;
+    }
+    
+}//end brand A smart bulb
