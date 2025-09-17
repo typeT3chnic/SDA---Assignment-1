4372a0baed7c4f2118bfb5e47a9bffaa64ed3d57
typeT3chnic <jaydentm1711@gmail.com>
2025-09-17 10:26:38 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,21 +4,6 @@ public interface SmartBulb {
     int getPowerUsage();
 }
 
-//BrandABulb
-class BrandABulb implements SmartBulb{
-    private int pwrUsage;
-    public BrandABulb() {
-    }
-    
-    public void setPowerUsage(int p){
-        this.pwrUsage = p;
-    }
-    
-    public int getPowerUsage(){
-        return this.pwrUsage;
-    }
-    
-}//end brand A smart bulb
 
 
 // SmartLock.java
