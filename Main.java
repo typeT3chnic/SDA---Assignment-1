1ef682f3f46da5dc64f954002c40d9c1dc0f4f88
typeT3chnic <jaydentm1711@gmail.com>
2025-09-17 10:27:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,18 +13,3 @@ public interface SmartLock {
 }
 
 
-// BrandALock 
-class BrandALock implements SmartLock{
-    private int batt;
-    public BrandALock(){
-
-    }
-
-    public void setBatteryConsumption(int b){
-        this.batt = b;
-    }
-
-    public int getBatteryConsumption(){
-        return this.batt;
-    }
-}//end Brand A Lock
