f6fcd449141fbfd05230310d7539575f508868f7
Jayden M. <jaydentm1711@gmail.com>
2025-09-19 14:44:26 -0400

    Update BrandBLock.java

--- BrandBLock.java
+++ BrandBLock.java
@@ -5,17 +5,14 @@ class BrandBLock implements SmartLock {
     public BrandBLock() {
     }
 
-    @Override
     public void setBatteryConsumption(int b) {
         this.batt = b;
     }
 
-    @Override
     public int getBatteryConsumption() {
         return this.batt;
     }
 
-    @Override
     public void create() {
         System.out.println("Creating Brand B smart lock.");
     }
