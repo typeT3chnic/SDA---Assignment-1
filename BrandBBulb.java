1843a41ba5cbd90de6cff8e692892fe62d36ec84
Jayden M. <jaydentm1711@gmail.com>
2025-09-19 14:43:55 -0400

    Update BrandBBulb.java

--- BrandBBulb.java
+++ BrandBBulb.java
@@ -4,18 +4,15 @@ class BrandBBulb implements SmartBulb {
 
     public BrandBBulb() {
     }
-
-    @Override
+  
     public void setPowerUsage(int p) {
         this.pwrUsage = p;
     }
 
-    @Override
     public int getPowerUsage() {
         return this.pwrUsage;
     }
 
-    @Override
     public void create() {
         System.out.println("Creating Brand B smart bulb.");
     }
