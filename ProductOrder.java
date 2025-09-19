352a288454dcc023ba62c47f4a620258cbad5e8e
Jayden M. <jaydentm1711@gmail.com>
2025-09-19 14:52:46 -0400

    Create ProductOrder.java

---
+++ ProductOrder.java
@@ -0,0 +1,47 @@
+//client class
+public class ProductOrder {
+    private SmartBulb bulb;
+    private SmartLock lock;
+    private String pro;
+
+    public ProductOrder(SmartDeviceFactory factory, String product){
+        this.pro = product;
+
+        //call createBulb() method if product is bulb
+        if(product.equalsIgnoreCase("bulb"))
+            this.bulb = factory.createBulb();
+
+        //call createLock() method if product is lock
+        else
+            this.lock = factory.createLock();
+    }
+
+    //call behaviour encapsulation method
+    public void createProducts(){
+        if(this.pro.equalsIgnoreCase("bulb"))
+            this.bulb.create();
+
+        else
+            this.lock.create();
+    }
+
+    //set Power usage for smart bulb
+    public void setPwrUsage(int p){
+        this.bulb.setPowerUsage(p);
+    }
+
+    //set Battery usage for smart lock
+    public void setBatteryCons(int b){
+        this.lock.setBatteryConsumption(b);
+    }
+
+    //return power usage
+    public void getPwrUsage(){
+        System.out.println("The power usage of this smart bulb is " + this.bulb.getPowerUsage() + " W.");
+    }
+
+    //return battery consumption
+    public void getBatteryCons(){
+        System.out.println("The battery consumption of this smart lock is " + this.lock.getBatteryConsumption() + " Ah.");
+    }
+}
