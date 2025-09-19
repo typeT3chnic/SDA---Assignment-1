389cf8970d739fa74c2f203009a4e1dca30c4c6d
Jayden M. <jaydentm1711@gmail.com>
2025-09-19 19:43:20 -0400

    Update TestDriver.java

--- TestDriver.java
+++ TestDriver.java
@@ -1,5 +1,12 @@
+/*
+* Jayden Mallari (100927861) & Rabab Raza (100932792)
+* CRN 45740
+* Group 4
+* TestDriver class that runs code
+*/
 public class TestDriver {
     public static void main(String[] args) {
+
         //Brand A Smart Bulb example
         SmartDeviceFactory factory = new BrandAFactory();
         ProductOrder fstOrder = new ProductOrder(factory, "bulb");
