130e335088a67aca56ada2f992a0009dd2ccb45b
Jayden M. <jaydentm1711@gmail.com>
2025-09-19 19:44:34 -0400

    Update TestSmartLock.java

--- TestSmartLock.java
+++ TestSmartLock.java
@@ -1,16 +1,23 @@
+/*
+ * Jayden Mallari (100927861) & Rabab Raza (100932792)
+ * CRN 45740
+ * Group 4
+ * TestSmartLock.java tests the behaviour encapsulation methods of all instances of classes implementing the SmartLock interface
+ */
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSmartLock {
 
-    @Test
+    @Test//tests setting the battery consumption and retrieving the correct value for Brand A Smart Locks
     void testSetAndGetBatteryConsumption_BrandA(){
         SmartLock lock = new BrandALock();
         lock.setBatteryConsumption(6);
         assertEquals(6, lock.getBatteryConsumption());
     }
 
-    @Test
+    @Test//tests setting the battery consumption and retrieving the correct value for Brand B Smart Locks
     void testSetAndGetBatteryConsumption_BrandB(){
         SmartLock lock = new BrandBLock();
         lock.setBatteryConsumption(8);
